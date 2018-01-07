e32cd2f115d77684d79204e4981c76a016d60dc8
cmoncayo <cmoncayo@espol.edu.ec>
2018-01-07 13:19:39 -0500

    add

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scle=1.0">
+ 
+ <link rel="stylesheet" href="index.css">
+ <link rel="stylesheet" type="text/css" href="estilo.css">
+
+  <title>Inicio</title>
+
+</head>
+<body>
+<div>
+<header>
+
+	
+	<nav class="navbar navbar-default" role="navigation">
+	
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav navbar-left">
+        <img src="LOGOcamaron.jpg">
+        </ul>
+        <ul class="nav navbar-nav navbar-center">
+            <li><a href="#">Inicio</a></li>
+  
+            <li class="dropdown">
+            	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
+            		Guarderías <span class="caret"></span>
+            	</a>
+            	<ul class="dropdown-menu" role="menu">
+            		<li><a href="#">Localizar Guarderías</a></li>
+            		<li><a href="#">Ver Actividades</a></li>
+            		<li><a href="#">Sé parte</a></li>
+            	</ul>
+            </li>
+            <li><a href="sugerencias.html">Contactenos</a></li>
+           <li><a href="">Quienes somos</a></li>
+
+        </ul>
+        <form action="" class="navbar-form navbar-right" role="search">
+        	<div class="form-group">
+        		<input type="text" class="form-control" placeholder="buscar">
+        	</div>
+        	<button type="submit" class="btn btn-primary">
+        		<span class="glyphicon glyphicon-search"></span>
+        	</button>
+        </form>
+        <ul class="nav navbar-nav navbar-right">
+           <li><a href="login.html">Login</a></li>
+             
+            </ul>
+    
+       
+    </div>
+</nav>
+		
+	
+</header>
+
+<section class="jumbotron jumbotron-km" >
+	<div class="container" >
+		
+	</div>
+</section>
+</div>
+
+<section class="main container">
+
+  
+ </section>
+ 
+  <script src="https://code.jquery.com/jquery-latest.js"></script>
+<!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+</body>
+</html>
