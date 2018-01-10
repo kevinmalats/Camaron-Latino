d0983ebe6dafe236fc6fe0f82ee3659bf8f4cd5f
ichigoblack <zarakikun1991@gmail.com>
2018-01-10 07:05:26 -0500

    cambio

--- index.php
+++ index.php
@@ -7,82 +7,57 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scle=1.0">
  
- <link rel="stylesheet" href="css/index.css">
- <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    
+ <link rel="stylesheet" href="css/cuerpo.css">
+ <link rel="stylesheet" href="css/in.css">
+
 
   <title>Inicio</title>
 
 </head>
 <body>
-<div>
-<header>
-
-	
-	<nav class="navbar navbar-default" role="navigation">
-	
-    <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-        </button>
-    </div>
-    <div class="navbar-collapse collapse">
-        <ul class="nav navbar-nav navbar-left">
-            <a href="index.php"><img src="img/camaronlogofinal.png" class="logo"></a>
-        </ul>
-        <ul class="nav navbar-nav navbar-center">
+          </button>
+          <a  href="index.php"><img class="peqe" src="img/camaronlogofinal.png"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
             <li><a href="index.php">Inicio</a></li>
-            <li class="dropdown">
-            	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
-            		Servicios <span class="caret"></span>
-            	</a>
-            	<ul class="dropdown-menu" role="menu">
-            		<li><a href="paginas/servicios.html">electromecanico</a></li>
-                    <li><a href="paginas/servicios2.html">civil</a></li>
-                    <li><a href="paginas/servicios3.html">otros</a></li>
-            		<li><a href="#">añadir</a></li>
-            	</ul>
-            </li>
-            <li class="dropdown">
-            	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">
-            		Productos <span class="caret"></span>
-            	</a>
-            	<ul class="dropdown-menu" role="menu">
-            		<li><a href="paginas/productos.html">alimenticio</a></li>
-                    <li><a href="paginas/productos2.html">larvas</a></li>
-                    <li><a href="paginas/productos3.html">otros</a></li>
-            		<li><a href="">añadir</a></li>
-            	</ul>
-            </li>
+            <li><a href="paginas/nosotros.html">Quienes somos</a></li>
             <li><a href="paginas/sugerencias.html">Contactenos</a></li>
-           <li><a href="paginas/nosotros.html">Quienes somos</a></li>
-
-        </ul>
-        <form action="" class="navbar-form navbar-right" role="search">
-        	<div class="form-group">
-        		<input type="text" class="form-control" placeholder="buscar">
-        	</div>
-        	<button type="submit" class="btn btn-primary">
-        		<span class="glyphicon glyphicon-search"></span>
-        	</button>
-        </form>
-        <ul class="nav navbar-nav navbar-right">
-           <li><a href="paginas/login.html">Login</a></li>
-        </ul>
-    </div>
-</nav>
-		
-	
-</header>
-
-<section class="jumbotron jumbotron-km" >
-	<div class="container" >
-		
-	</div>
-</section>
-</div>
-
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Servicios <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="paginas/servicios.html">electromecanico</a></li>
+                <li><a href="paginas/servicios2.html">civil</a></li>
+                <li><a href="paginas/servicios3.html">otros</a></li>
+                <li><a href="#">añadir</a></li>
+              </ul>
+            </li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Productos <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="paginas/productos.html">alimenticio</a></li>
+                <li><a href="paginas/productos2.html">larvas</a></li>
+                <li><a href="paginas/productos3.html">otros</a></li>
+                <li><a href="">añadir</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li class="active"><a href="paginas/login.html">Login<span class="sr-only">(current)</span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
 <section class="main container">
 
   
