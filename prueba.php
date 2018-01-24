f6b12e95ea447456e5a25dc3affc4b5d81b9d06d
ichigoblack <zarakikun1991@gmail.com>
2018-01-24 10:39:32 -0500

    subir

--- prueba.php
+++ prueba.php
@@ -1,29 +1,10 @@
 <?php
-class rol
-{
-    private $idrol;
-    private $nombre;
-    private $descripcion;
-
-     function __construct() {
-     }
-    
-     function setCredencial($Credencial){
-       $this->Credencial = $Credencial;
-     } 
-     function getCredencial(){
-       return $this->Credencial;
-     }
-     function setClave($clave){
-       $this->clave = $clave;
-     } 
-     function getClave(){
-       return $this->clave;
-     } 
-}
-$usuario = "carlos";
-$clave = "1234";
-$Palabrerio = new credencial($usuario,$clave); 
-echo "hola"."<br>";
-echo "el usuario es: ". $Palabrerio->getUsuario()."";
-?> 
+require_once("credencial.php");
+$usu = "dani";
+$cla = "1234";
+$test = new credencial();
+$test->setUsuario($usu);
+$test->setClave($cla);
+echo "el usuario se llama: ". $test->getUsuario();
+echo "la contraseña es: " .$test->getClave;
+?>
