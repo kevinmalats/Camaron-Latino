92a873a852bb2146e1892c5411050f10dc12f84b
jmmosque <jmmosque@espol.edu.ec>
2018-01-26 11:36:54 -0500

    TRavis Todos

---
+++ pruebarada.php
@@ -0,0 +1,13 @@
+<?php
+require_once("sugerencia.php");
+$idsugerencia = "compras";
+$idnombre = "larvas";
+$idmensaje = " recomendable";
+$test = new sugerencia();
+$test->setIdsugerencia($idsugerencia);
+$test->setNombre($idnombre);
+$test->setMensaje($idmensaje);
+echo "El ID sugerencia es: ". $test->getIdsugerencia();
+echo "El Id del nombre es: " .$test->getNombre();
+echo "EL mensaje es es: " .$test-> getMensaje();
+?>
