159b7e3353a69c0b834e8a2eb41f5f4d9aa2cbdd
ichigoblack <zarakikun1991@gmail.com>
2018-01-27 18:02:32 -0500

    phpcredenciales

---
+++ login.php
@@ -0,0 +1,137 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Blog Single | Corlate</title>
+    
+    <!-- core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/prettyPhoto.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <link href="css/propio.css" rel="stylesheet">
+    
+    <!--[if lt IE 9]>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="images/ico/apple-touch-icon-57-precomposed.png">
+</head><!--/head-->
+
+<body>
+
+    <header id="header">
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php"><img src="images/logo.png" alt="logo"></a>
+                </div>
+                
+                 <div class="collapse navbar-collapse navbar-right">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="index.php">Inicio</a></li>
+                        <li><a href="about-us.php">Nosotros</a></li>
+                        <li><a href="servicios.php">Servicios</a></li>
+                        <li><a href="productos.php">Productos</a></li> 
+                        <li><a href="contact-us.php">Contactenos</a></li> 
+                        <!--<li><a href="login.html">Login</a></li>-->
+                         <?php
+                            if ($_SESSION){
+                        ?>
+                            <li><a href="logout.php">Logout</a></li>
+                        <?php
+                                if ($_SESSION["perfil"]=="admin"){
+                        ?>
+                            <li><a href="index.php"><strong>Bienvenido:  </strong> <?php echo $_SESSION['id'];?></a></li>
+                        <?php
+                                }else{
+                        ?>
+                            <li><a title="Bienvenido" ><strong>Bienvenido:  </strong> <?php echo $_SESSION['id'];?></a></li>
+                        <?php
+                                }
+                            }else{
+                        ?>
+                            <li><a href="login.php">Login</a></li>
+                        <?php
+                            }
+                        ?>
+                    </ul>
+                </div>
+            </div><!--/.container-->
+        </nav><!--/nav-->
+    </header><!--/header-->
+    <section id="tamasec"><h2 class="center2">Inicie Sesion</h2>
+      <div class="containerlogin">
+        <div class="card card-login mx-auto mt-5">
+          <div class="card-header">Login</div>
+            <div class="card-body">
+                <form action="validacredencial.php" method="post">
+                    <div class="form-group">
+                        <label>Usuario</label>
+                        <input class="form-control estiloborder" placeholder="" type="text" name="usu">
+                    </div>
+                    <div class="form-group">
+                        <label >Contraseña</label>
+                        <input class="form-control" type="password" placeholder="" type="text" name="cla">
+                    </div>
+                    <a class="btn btn-primary btn-block" type="submit">Login</a>
+                    <button type="submit"> ENVIAR </button>
+                </form>
+                <div class="text-center">
+                    <br>
+                    <a class="d-block small mt-3" href="registre.php">Registre una Cuenta</a>
+                    <a class="d-block small" href="forgot.php">Olvidó contraseña?</a>
+                </div>
+          </div>
+        </div>
+      </div>
+    </section>     
+    <br>
+        <footer id="footer" class="midnight-blue">
+            <div class="container">
+                <div class="row">
+                      <div class="col-sm-6">
+                    &copy; 2013 <a target="_blank" href="http://shapebootstrap.net/" title="Free Twitter Bootstrap WordPress Themes and HTML templates">ShapeBootstrap</a>. All Rights Reserved.
+                </div>
+                   
+                    <div class="col-sm-6 col-xs-8">
+                       <div class="social">
+                            <ul class="social-share">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li> 
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                <li><a href="#"><i class="fa fa-skype"></i></a></li>
+                            </ul>
+                       </div>
+                    </div>
+                </div>
+            </div><!--/.container-->
+    </footer><!--/#footer-->
+
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/wow.min.js"></script>
+</body>
+</html>
