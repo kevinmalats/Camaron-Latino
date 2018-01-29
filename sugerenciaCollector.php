ba65b733a6c2ef7b1b102750b10090482368d3d8
davidr123 <davidradaalejandro@gmail.com>
2018-01-28 22:10:41 -0500

    todos

--- sugerenciaCollector.php
+++ sugerenciaCollector.php
@@ -5,7 +5,7 @@ include_once('Collector.php');
 class sugerenciaCollector extends Collector
 {
 
-function showsugerencia(){
+function showSugerencia(){
 $rows = self::$db->getRows("SELECT * FROM sugerencia");
 $arraysugerencia= array();
 foreach($rows as $c){
@@ -14,11 +14,58 @@ array_push($arraysugerencia, $aux);
 }
 return $arraysugerencia;
 }
-function deletesugerencia($id){
-echo "processing delete id:".$id ."<br>";
-$deleterow=self::$db->deleteRow(DELETE FROM public.sugerencia WHERE id_sugerencia= ?", arrSSay("{$id}"))
-echo "delete completed<br>";
+
+
+function showSugerencias(){
+$rows = self::$db->getRows("SELECT * FROM sugerencia");
+$arraysugerencia= array();
+foreach($rows as $c){
+$aux = new sugerencia($c{'id_sugerencia'}, $c{'fecha'}, $c{'mensaje'}, $c{'id_usuario'});
+
 }
+return $aux;
+
+
+function deleteSugerencia($id){
+        echo "processing delete id:". $id ."<br>";
+        $deleterow = self::$db->deleteRow("DELETE FROM public.credencial WHERE id_credencial= ?", array("{$id}"));
+        echo "delete completed<br>";
+    }
+    
+ function consultarSugerencia($id_usuario, $mensaje) {
+    $rows = self::$db->getRows("SELECT * FROM public.credencial WHERE usuario=? AND clave=?  ", array("{$id_usuario}","{$mensaje}"));        
+    $Sugerencia = new sugerencia();
+    foreach ($rows as $c){ 
+        $ObjSugerencia->setIdCredencial($c{'id_sugerencia'});
+        $ObjSugerencia->setIdsugerencia($c{'id_usuario'});
+        $ObjSugerencia->setMensaje($c{'mensaje'});
+    }
+    return $ObjSugerencia;        
+    }
+    
+    
+    function comprobarSugerencia($id_usuario) {
+    $rows = self::$db->getRows("SELECT * FROM public.credencial WHERE usuario=? ", array("{$id_usuario}"));        
+    $ObjSugerencia = new sugerencia();
+    $ObjSugerencia->setIdsugerencia($rows[0]{'id_sugerencia'});
+    $ObjSugerencia->setUsuario($rows[0]{'id_usuario'});
+    $ObjSugerencia->setMensaje($rows[0]{'mensaje'});
+    return $ObjSugerencia;        
+    }
+    
+    
+     function crearcredencial($id_usuario,$mensaje){
+        $insertarrow = self::$db->insertRow("INSERT INTO public.credencial (usuario,mensaje) VALUES (?,?)", array ("{$id_usuario}","{$mensaje}"));
+    }
+}
+    $objSugerencia = new sugerencia();
+    
+    $objSugerencia =  $objSugerencia->comprobarSugerencia('frada');
+    echo "id: ". $sugerencia->getIdsugerencia()."<br>";
+    echo "id: ". $sugerencia->getIdUsuario()."<br>";
+    echo "id: ". $sugerencia->getMensaje()."<br>";
+    print_r($sugerencia);
+
 }
 ?>
 
--- tipo_productoCollector.php
+++ tipo_productoCollector.php
@@ -5,20 +5,70 @@ include_once('Collector.php');
 class tipoproductoCollector extends Collector
 {
 
-function showtipoproducto(){
+function showtipoProductos(){
 $rows = self::$db->getRows("SELECT * FROM tipo_producto");
 $arraytipoproducto= array();
 foreach($rows as $c){
-$aux = new tipoproducto($c{'id_tipo_producto'}, $c{'nombre'}, $c{'descripcion'});
+$aux = new tipoProducto($c{'idTipoProducto'}, $c{'nombre'}, $c{'descripcion'});
 array_push($arraytipoproducto, $aux);
 }
 return $arraytipoproducto;
 }
+
+
+  function showtipoProducto($id) {
+      $rows = self::$db->getRows("SELECT * FROM public.tipoProducto WHERE id_credencial= ?", array("{$id}"));        
+      $aux = new tipoProducto();
+      foreach ($rows as $c){ 
+       $aux->setIdTipoProducto($c{'idTipoProducto'});
+       $aux->setNombreo($c{'$nombre'});
+       $aux->setDescripcion($c{'$descripcion'});
+    }
+      return $aux;        
+   } 
+
+
 function deletetipoproducto($id){
 echo "processing delete id:".$id ."<br>";
-$deleterow=self::$db->deleteRow(DELETE FROM public.tipo_producto WHERE id_tipo_producto= ?", arrSSay("{$id}"))
+$deleterow=self::$db->deleteRow(DELETE FROM public.tipoProducto WHERE idTipoProducto= ?", arrSSay("{$id}"))
 echo "delete completed<br>";
 }
+
+
+ function consultarProducto($nombre, $descripcion) {
+    $rows = self::$db->getRows("SELECT * FROM public.tipoProducto WHERE nombre=? AND descripcion=?  ", array("{$nombre}","{$descripcion}"));        
+    $ObjProducto = new tipoProducto();
+    foreach ($rows as $c){ 
+        $ObjTipoProducto->setIdTipoProducto($c{'idTipoProducto'});
+         $ObjTipoProducto->setNombre($c{'nombre'});
+         $ObjTipoProducto->setDescripcion($c{'descripcion'});
+    }
+    return $ObjProducto;        
+    }
+    
+    
+    function comprobarCredencial($nombre) {
+    $rows = self::$db->getRows("SELECT * FROM public.credencial WHERE usuario=? ", array("{$nombre}"));        
+    $ObjTipoProducto = new tipoProducto();
+     $ObjTipoProducto->setIdTipoProducto(($rows[0]{'idTipoProducto'});
+     $ObjTipoProducto->setNombre($rows[0]{'nombre'});
+     $ObjTipoProducto->setDescripcion($rows[0]{'descripcion'});
+    return  $ObjTipoProducto;        
+    }
+    
+     function crearcredencial($nombre,$descripcion){
+        $insertarrow = self::$db->insertRow("INSERT INTO public.tipoProducto (nombre,descripcion) VALUES (?,?)", array ("{$nombre}","{$descripcion}"));
+    }
+}
+    $objTipoproducto = new tipoproductoCollector();
+    
+    $Tipoproducto = $objTipoproducto->comprobarTipoproducto('frada');
+    echo "id: ". $Tipoproducto->getIdTiPoproducto()."<br>";
+    echo "id: ". $Tipoproducto->getNombre()."<br>";
+    echo "id: ". $Tipoproducto->getDescripcion()."<br>";
+    print_r($Tipoproducto);
+
+
 }
 ?>
 
